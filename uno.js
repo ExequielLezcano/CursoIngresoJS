18658b2555a9f2d3e8085e59b4cd7e005a25fe71
ExequielLezcano <36281665+ExequielLezcano@users.noreply.github.com>
2018-02-09 21:08:32 -0300

    PARCIAL
    1º Practicalo

---
+++ uno.js
@@ -0,0 +1,17 @@
+
+function Mostrar()
+{
+	var base;
+	var perimetro;
+
+	base = document.getElementById('laBase').value;
+
+	base = parseInt(base);
+
+	perimetro = 4
+
+	alert("El perimetro del cuadrado es " + base * perimetro);
+
+
+
+}
