2158ba2694b3b2701d9222760e77d93f1bb444e9
ExequielLezcano <36281665+ExequielLezcano@users.noreply.github.com>
2018-02-22 22:14:54 -0300

    Add files via upload

---
+++ jsIteraciones (1).js
@@ -0,0 +1,13 @@
+function Mostrar()
+{
+	//alert('iteración while');
+    var contador = 1;
+
+    while(contador < 11)
+    {
+    	document.write(contador);
+
+    	contador = contador + 1;
+    }
+
+}//FIN DE LA FUNCIÓN
