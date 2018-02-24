38887c6dea40324cfcbb77ff3ca22bbf38cbe717
ExequielLezcano <36281665+ExequielLezcano@users.noreply.github.com>
2018-02-23 21:48:50 -0300

    Add files via upload

---
+++ jsIteracionesFor (1).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+   for(var i = 1; 1 < 11; i = i + 1)
+   {
+   	  console.log(i);
+   }
+}
+
+/*
+    for(var cantidad = 0; cantidad < 5; cantidad = cantidad + 1)
+    {   se ejecuta una    sigue aqui     al llegar al final de las
+    	                                 llaves ejecuta este
+    	vez
+	   
+    }
+*/
