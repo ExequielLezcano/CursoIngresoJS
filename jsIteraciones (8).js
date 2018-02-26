beaab7a23df1e0c674017cc559a2289830fdd4ea
ExequielLezcano <36281665+ExequielLezcano@users.noreply.github.com>
2018-02-26 11:46:30 -0300

    Add files via upload

--- jsIteraciones (8).js
+++ jsIteraciones (8).js
@@ -8,34 +8,34 @@ function Mostrar()
 	var respuesta = 'si';
 
 	while(respuesta == "si")
-	{
-		contador = contador + 1
-
-		numero = prompt("Ingrese un numero");
-		numero = parseInt(numero);
-
-		while(isNaN(numero))
 		{
-			numero = prompt("Error, ingrese un numero");
+			contador = contador + 1;
+
+			numero = prompt("Ingrese un numero");
+
 			numero = parseInt(numero);
-		}
+
+			while(isNaN(numero))
+			{
+				numero = prompt("Error, ingrese un numero");
+
+				numero = parseInt(numero);
+			}
+
 			if(numero > 0)
 			{
 				positivo = positivo + numero;
 			}
-				else
-				{
-					if(numero < 0)
-					{
-						negativo = negativo * numero;
-					}
-				}
+			 else
+			 {
+			 	negativo = negativo * numero;
+			 }
 
-		respuesta = prompt("Si desea continuar, por favor ingrese la palabra, si");
-	}
+			respuesta = prompt("Si desea continuar con este ejercicio, escriba si");
+		}
 
 
-	document.getElementById('suma').value = positivo;
-	document.getElementById('producto').value = negativo;
+document.getElementById('suma').value = positivo;
+document.getElementById('producto').value = negativo;
 
 }//FIN DE LA FUNCIÓN
