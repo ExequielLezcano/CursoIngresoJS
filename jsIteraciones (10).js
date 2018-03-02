eff86429645995e0f4a4ba3ad57686bc4e112ba6
ExequielLezcano <36281665+ExequielLezcano@users.noreply.github.com>
2018-03-02 19:45:49 -0300

    Add files via upload

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -1,52 +1,54 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var contador = 0;
 	//declarar contadores y variables 
-	var positivo = 0;
-	var negativo = 0;
-	var cantidadpositivo;
-	var cantidadnegativo;
-	var cantidadceros;
-	var cantidadpar;
 	var numero;
+	var acumuladorN = 0;
+	var acumuladorP = 0;
+	var contadorP = 0;
+	var contadorN = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	
 	var respuesta = "si";
 
 	while(respuesta != "no")
-	{
-		contador = contador + 1;
-
-		numero = prompt("Ingrese un numero");
-		numero = parseInt(numero);
-
-		while(isNaN(numero))
 		{
-			numero = prompt("Error, ingrese un numero");
+		    numero = prompt("Ingrese un numero");
+			
 			numero = parseInt(numero);
+
+				if(numero > 0 )
+				{
+					contadorP++;
+					acumuladorP += numero;
+				}
+				else
+				{
+					if(numero < 0 )
+					{
+						contadorN++;
+						acumuladorN += numero;
+					}
+					else
+					{
+						contadorCeros++;
+					}
+				}
+				if(numero % 2 == 0 && numero != 0)
+				{
+					contadorPares ++;
+				}
+
+		    respuesta = prompt("Ingrese si, si desea continuar");
 		}
-		 if(numero > 0)
-		 {
-		 	cantidadpositivo = cantidadpositivo + 1;
-		 	positivo = positivo + numero;
-		 }
-		  else if (numero < 0)
-		  {
-            cantidadnegativo = cantidadnegativo + 1;
-            negativo = negativo + numero;
-		  }
-		   else
-		   {
-		   	  cantidadceros = cantidadceros + 1;
-		   }
 
-		    if(numero % 2 == 0 && numero != 0)
-		    {
-               cantidadpar = cantidadpar + 1;
-		    }
+	document.write("Negativos " + acumuladorN + " Positivos " + acumuladorP + " Cant de positivos " + contadorP + " Cant de Negativos " + contadorN + " cantidad de ceros  " + contadorCeros + " cantidad de pares " + contadorPares + " promedio de positivos " + (acumuladorP / contadorP) + " promedio de negativos " + (acumuladorN / contadorN) + " diferencia de positivos y negativos " + (acumuladorP - acumuladorN));	
 
-		respuesta = prompt("Si desea continuar escriba la palabra, si");
-	
-	}
+	/* alert(acumuladorP / contadorP);
+	alert(acumuladorN / contadorN);
+	alert(acumuladorP - acumuladorN); */
 
 
 
